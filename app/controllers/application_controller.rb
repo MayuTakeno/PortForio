e927148ec7f17727b70543ff138011df03684e47
EC2 Default User <ec2-user@ip-172-31-17-27.ec2.internal>
2022-06-30 10:43:12 +0000

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_public_sign_in_path_for(resource)
     public_root_path
   end
@@ -10,6 +12,12 @@ class ApplicationController < ActionController::Base
 
   def after_public_sign_out_path_for(resource)
     public_root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :assigned_to, :employee_code, :phone_number])
   end
 
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,5 +4,8 @@ class Employee < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+ def name
+   first_name + last_name
+ end
 
 end
