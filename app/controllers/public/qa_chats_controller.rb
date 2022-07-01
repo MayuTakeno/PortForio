1c314b17f2bd4e329d413fb7d003a692efc1f542
EC2 Default User <ec2-user@ip-172-31-17-27.ec2.internal>
2022-07-01 02:25:00 +0000

    chat機能追加

--- app/controllers/public/qa_chats_controller.rb
+++ app/controllers/public/qa_chats_controller.rb
@@ -1,4 +1,34 @@
 class Public::QaChatsController < ApplicationController
   def show
+    # 社員の情報を一つ取り出す
+    @employee = Employee.find(params[:id])
+    # 現在ログインしている社員のemployee_roomsにあるroom_idを配列で取得
+    rooms = current_employee.employee_rooms.pluck(:room_id)
+    # userIDがチャット相手の社員のIDと一致する # room_idが、上記のroomsのどれか一つに一致するレコード
+    user_rooms = UserRooms.find_by(user_id: @employee.id, room_id: rooms)
+    # employee_roomが空でなかった場合
+    unless employee_rooms.nil?
+      # employee_roomのroomを続行
+      @room = employee_rooms.room
+    # employee_roomsが空だった場合
+    else
+      # 新しく部屋を作る
+      @room = Room.new
+      # 新しく作った部屋をDBに保存
+      @room.save
+      # 現在ログインしている社員と相手の社員分のemployee_roomを作成
+      EmployeeRoom.create(employee_id: current_employee.id, room_id: @room.id)
+      EmployeeRoom.create(employee_id: @employee.id, room_id: @room.id)
+    end
+    # @room内のchatsを取得
+    @chats = @room.chats
+    # room_idにchatを新規作成
+    @chat = Chat.new(room_id: @room.id)
+  end
+
+  private
+
+  def qa_chat_params
+    params.require(:qa_chat).permit(:title, :message, :room_id)
   end
 end
