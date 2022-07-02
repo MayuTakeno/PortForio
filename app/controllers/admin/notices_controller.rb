71ac78194cf8b49582957a190348f35cf72cbd35
EC2 Default User <ec2-user@ip-172-31-17-27.ec2.internal>
2022-07-02 19:25:39 +0000

    編集

--- app/controllers/admin/notices_controller.rb
+++ app/controllers/admin/notices_controller.rb
@@ -1,13 +1,36 @@
 class Admin::NoticesController < ApplicationController
+  before_action :set_notice, only: [:show, :edit, :update, :destroy]
+
   def new
+    @notice = Notice.new
+  end
+
+  def create
+    @notice = Notice.new(notices_params)
+    if @notice.save
+      redirect_to admin_notices_path
+    else
+      render :new
+    end
   end
 
   def index
+    @notices = Notice.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def notices_params
+    params.require(:notice).permit(:title, :body, :caption, :image)
+  end
+
+  def set_notice
+    @notice = Notice.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_175508) do
+ActiveRecord::Schema.define(version: 2022_07_01_012839) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -85,13 +85,6 @@ ActiveRecord::Schema.define(version: 2022_07_02_175508) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_employees_on_email", unique: true
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
-  end
-
-  create_table "news", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "notices", force: :cascade do |t|
