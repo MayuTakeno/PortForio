cce7e334c9a817bf0d8113a47fa68cd82f97cb0f
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-07-06 03:11:44 +0000

    order追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,6 +7,10 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     if @order.save
       @cart_items = current_employee.cart_items
+      @cart_items.each do |cart_item|
+        ProductOrder.create!(
+          
+        )|
       redirect_to public_order_path(order)
     else
       render :request.referer
@@ -22,6 +26,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:billing_amount, :payment_method, :status, :name, :phone_number)
+    params.require(:order).permit(:billing_amount, :payment_method, :status, :name, :phone_number, :employee_id)
   end
+
 end
