a8c5efe449f74b8f4ac15f1d92ad2eca86093675
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-07-07 07:26:00 +0000

    追加

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,15 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    blog = Blog.find(params[:blog_id])
-    favorite = current_employee.favorites.new(blog_id: blog.id)
-    favorite.save
-    redirect_to public_blog_path(blog)
+    @blog = Blog.find(params[:blog_id])
+    @favorite = current_employee.favorites.new(blog_id: @blog.id)
+    @favorite.save
   end
 
   def destroy
-    blog = Blog.find(params[:blog_id])
-    favorite = current_employee.favorites.find_by(blog_id: blog.id)
-    favorite.destroy
-    redirect_to public_blog_path(blog)
+    @blog = Blog.find(params[:blog_id])
+    @favorite = current_employee.favorites.find_by(blog_id: @blog.id)
+    @favorite.destroy
   end
 
 end
